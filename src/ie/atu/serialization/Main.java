32ae1b6c1b06616245ee3a838a00e02c4a90af14
Min Kyaw <mintheinkyawprd@gmail.com>
2026-03-26 12:30:52 +0000

    Add Book class and Main serialization example

---
+++ src/ie/atu/serialization/Main.java
@@ -0,0 +1,35 @@
+// File: Main.java
+package ie.atu.serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a book
+        Book book = new Book("Java Fundamentals", "John Doe", 29.99);
+        System.out.println("Original Book: " + book);
+        
+        // Serialize
+        try (FileOutputStream fileOut = new FileOutputStream("resources/book.ser");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(book);
+            System.out.println("Book has been serialized");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Deserialize
+        try (FileInputStream fileIn = new FileInputStream("resources/book.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            Book deserializedBook = (Book) in.readObject();
+            System.out.println("Book has been deserialized");
+            System.out.println("Deserialized Book: " + deserializedBook);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
